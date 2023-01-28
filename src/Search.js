c0fa1567aab22f023bab479e39b4cdeee1b99066
NChornenka <88328438+NChornenka@users.noreply.github.com>
2023-01-27 19:55:46 -0500

    Added Search engine

---
+++ src/Search.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Search.css";
+import axios from "axios";
+// import bootsrtap from "bootstrap";
+
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  function updateCity(event) {
+    setCity(event.target.value);
+    console.log(city);
+  }
+
+  function showTemperature(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      header: response.data.name
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "d682095eed5d0971ffeaf2a35ab9e196";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showTemperature);
+    
+  }
+  let form = (
+    <form className="d-flex" role="search" onSubmit={handleSubmit}>
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        onChange={updateCity}
+      />
+      <button className="btn btn-outline-success ms-2" type="submit">
+        Search
+      </button>
+    </form>
+  );
+
+  let text = (
+    <div>
+      <h1>{weather.header}</h1>
+      <ul>
+        <li>Temperature: {weather.temperature} °C </li>
+        <li>Description: {weather.description}</li>
+        <li>Humidity: {weather.humidity} %</li>
+        <li>Wind: {weather.wind} m/s</li>
+      </ul>
+    </div>
+  );
+
+  if (loaded) {
+    return (
+      <div className="Search">
+        <div>{form}</div>
+        <div>{text}</div>
+      </div>
+    );
+  } else {
+    return <div>{form}</div>;
+  }
+}
