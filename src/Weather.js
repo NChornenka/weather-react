99cc5c34c80304ad000e887a918eecb1969523b7
NChornenka <88328438+NChornenka@users.noreply.github.com>
2023-02-03 20:00:17 -0500

    Implemented Search function

--- src/Weather.js
+++ src/Weather.js
@@ -7,8 +7,9 @@ import WeatherData from './WeatherData';
 export default function Weather(props){
     const [ready, setReady] = useState(false);
     const [weatherData, setWeatherData] = useState({});
+    const [city, setCity] = useState(props.defaultCity);
 
-    function handleSubmit(response){
+    function handleResponse(response){
         setReady(true);
         setWeatherData(
             {
@@ -26,35 +27,54 @@ export default function Weather(props){
 
     }
 
+    function handleSubmit(event){
+        event.preventDefault();
+        if (city.length > 2){
+            search();
+                    } else {
+                        alert("Enter a city, please")
+                    }
+    }
+
+    function updateCity(event){
+        setCity(event.target.value);
+
+    }
+
+    function search(){
+
+        let apiKey = "d682095eed5d0971ffeaf2a35ab9e196";
+        let units = "metric";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+        axios.get(apiUrl).then(handleResponse);
+
+    }
+
 
     if (ready) {
         return (
     <div className='Weather'>
         <div className='container-weather'>
-            <form className="d-flex" role="search">
-        {/* <form className="d-flex" role="search" onSubmit={getCity}> */}
+            <form className="d-flex" role="search"
+            onSubmit={handleSubmit}>
             <input
             className="form-control"
             type="text"
             placeholder="City"
             aria-label="search"
             autoFocus="on"
-        // onChange={updateCity}
+            onChange={updateCity}
             />
-            <input className="btn btn-outline-success" type="submit"
-            Search
-            />
+            <input className="btn btn-outline-success" type="submit" value="Search" />
             </form>
     <WeatherData data={weatherData}/>
         </div>
     </div>
     )
 } else {
-    let apiKey = "d682095eed5d0971ffeaf2a35ab9e196";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleSubmit);
+    search();
     return "Loading...."
+
 }
 
 }
